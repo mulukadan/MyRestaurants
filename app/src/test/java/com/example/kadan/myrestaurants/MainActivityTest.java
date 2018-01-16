284e94583e6dd50c029b7975b6090510e235b9a5
mulukadan <kadan.md@gmail.com>
2018-01-16 18:41:20 +0300

    Added activity

---
+++ app/src/test/java/com/example/kadan/myrestaurants/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.example.kadan.myrestaurants;
+
+
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.TestCase.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+
+@RunWith(RobolectricTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+}
