222b784fcbf6193d1eefcdfc3c3de9c9c8429f38
mulukadan <kadan.md@gmail.com>
2018-01-17 11:19:13 +0300

    Added Butter slice

--- app/src/main/java/com/example/kadan/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/kadan/myrestaurants/MainActivity.java
@@ -2,25 +2,43 @@ package com.example.kadan.myrestaurants;
 
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+//    private Button mFindRestaurantsButton;
+//    private EditText mLocationEditText;
+//    private TextView mAppNameTextView;
+
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+//        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        //Using ButterKnife instead of the above
+        ButterKnife.bind(this);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/Capture_it_2.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
 
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,3 +52,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+
+
