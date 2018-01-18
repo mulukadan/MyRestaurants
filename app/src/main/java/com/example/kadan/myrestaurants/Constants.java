ac35e6bab1d56b680820f085a4d0881b9b482df2
mulukadan <kadan.md@gmail.com>
2018-01-18 16:47:42 +0300

    Adding APIs

---
+++ app/src/main/java/com/example/kadan/myrestaurants/Constants.java
@@ -0,0 +1,10 @@
+package com.example.kadan.myrestaurants;
+
+/**
+ * Created by kadan on 1/18/18.
+ */
+
+
+public class Constants {
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+}
