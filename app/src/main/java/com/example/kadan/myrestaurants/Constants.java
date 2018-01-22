d3eeb175e8a78fbf37c612f1ba3f5024aeed04b7
mulukadan <kadan.md@gmail.com>
2018-01-22 18:19:57 +0300

    API Search working

--- app/src/main/java/com/example/kadan/myrestaurants/Constants.java
+++ app/src/main/java/com/example/kadan/myrestaurants/Constants.java
@@ -7,4 +7,6 @@ package com.example.kadan.myrestaurants;
 
 public class Constants {
     public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
 }
---
+++ app/src/main/java/com/example/kadan/myrestaurants/YelpService.java
@@ -0,0 +1,99 @@
+package com.example.kadan.myrestaurants;
+
+import android.app.VoiceInteractor;
+import android.util.Log;
+
+/**
+ * Created by kadan on 1/18/18.
+ */
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Callback;
+import okhttp3.Call;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static com.example.kadan.myrestaurants.TryActivity.TAG;
+
+public class YelpService {
+    private  static OkHttpClient client = new OkHttpClient();
+
+    public static void findRestaurants(String location, Callback callback) {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .header("Authorization", "Bearer " + Constants.YELP_API_KEY)
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+
+    public ArrayList<Restaurant> processResults(Response response) {
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject yelpJSON = new JSONObject(jsonData);
+                JSONArray businessesJSON = yelpJSON.getJSONArray("businesses");
+                for (int i = 0; i < businessesJSON.length(); i++) {
+                    JSONObject restaurantJSON = businessesJSON.getJSONObject(i);
+                    String name = restaurantJSON.getString("name");
+                    String phone = restaurantJSON.optString("display_phone", "Phone not available");
+                    String website = restaurantJSON.getString("url");
+                    double rating = restaurantJSON.getDouble("rating");
+                    String imageUrl = restaurantJSON.getString("image_url");
+                    double latitude = restaurantJSON.getJSONObject("coordinates")
+                            .getDouble("latitude");
+                    double longitude = restaurantJSON.getJSONObject("coordinates")
+                            .getDouble("longitude");
+                    ArrayList<String> address = new ArrayList<>();
+                    JSONArray addressJSON = restaurantJSON.getJSONObject("location")
+                            .getJSONArray("display_address");
+                    for (int y = 0; y < addressJSON.length(); y++) {
+                        address.add(addressJSON.get(y).toString());
+                    }
+
+                    ArrayList<String> categories = new ArrayList<>();
+                    JSONArray categoriesJSON = restaurantJSON.getJSONArray("categories");
+
+                    for (int y = 0; y < categoriesJSON.length(); y++) {
+                        categories.add(categoriesJSON.getJSONObject(y).getString("title"));
+                    }
+                    Restaurant restaurant = new Restaurant(name, phone, website, rating,
+                            imageUrl, address, latitude, longitude, categories);
+                    restaurants.add(restaurant);
+                }
+
+                //Testing
+                ArrayList<String> address = new ArrayList<>();
+                address.add("218");
+                ArrayList<String> categories = new ArrayList<>();
+                categories.add("Cat 1");
+
+                Restaurant restaurant = new Restaurant("Test", "999", "www.test.com", 6,
+                        "nnnn", address, 22.3, 11.2, categories);
+                restaurants.add(restaurant);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return restaurants;
+    }
+
+}
